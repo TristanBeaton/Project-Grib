b5f87ba3ace84e3af28c4bf76d0cd4eef0c2296c
= <tristanbeaton@outlook.co.nz>
2017-09-23 15:15:03 +1200

    Tided Code

--- Project Grib/Grib API/GribMessage.swift
+++ Project Grib/Grib API/GribMessage.swift
@@ -40,36 +40,36 @@ class GribMessage {
             // Get the next section
             let section = try stream.readUI8()
             switch section {
-            case 1:
-                // Read the Identification Section
-                self.ids = try IdentificationSection(stream, length)
-                continue
-            case 2:
-                // Read the Local Use Section
-                self.loc = try LocalUseSection(stream, length)
-                continue
-            case 3:
-                // Read the Grid Definition Section
-                self.gds = try GridDefinitionSection(stream, length)
-                continue
-            case 4:
-                // Read the Product Definition Section
-                self.pds = try ProductDefinitionSection(stream, length)
-                continue
-            case 5:
-                // Read the Data Representation Section
-                self.drs = try DataRepresentationSection(stream, length)
-                continue
-            case 6:
-                // Read the Bit-Map Section
-                self.bitmap = try BitmapSection(stream, length)
-                continue
-            case 7:
-                // Read the Date Section
-                self.data = try DataSection(stream, length)
-                continue
-            default:
-                throw GribFileStreamError.InvalidSection(section)
+                case 1:
+                    // Read the Identification Section
+                    self.ids = try IdentificationSection(stream, length)
+                    continue
+                case 2:
+                    // Read the Local Use Section
+                    self.loc = try LocalUseSection(stream, length)
+                    continue
+                case 3:
+                    // Read the Grid Definition Section
+                    self.gds = try GridDefinitionSection(stream, length)
+                    continue
+                case 4:
+                    // Read the Product Definition Section
+                    self.pds = try ProductDefinitionSection(stream, length)
+                    continue
+                case 5:
+                    // Read the Data Representation Section
+                    self.drs = try DataRepresentationSection(stream, length)
+                    continue
+                case 6:
+                    // Read the Bit-Map Section
+                    self.bitmap = try BitmapSection(stream, length)
+                    continue
+                case 7:
+                    // Read the Date Section
+                    self.data = try DataSection(stream, length)
+                    continue
+                default:
+                    throw GribFileStreamError.InvalidSection(section)
             }
         }
         dump(self)
